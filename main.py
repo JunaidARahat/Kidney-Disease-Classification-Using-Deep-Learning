d437d449593b2f7516f68375d6b3c874700d85da
JunaidARahat <mjunaidrahat1990@gmail.com>
2024-05-18 22:27:01 +0500

    model evaluation wit mlflow added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from cnnClassifier import logger
 from cnnClassifier.pipeline.stage1_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage2_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipeline.stage3_model_training import ModelTrainingPipeline
-
+from cnnClassifier.pipeline.stage4_model_evaluation import EvaluationPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -35,6 +35,18 @@ try:
    model_trainer = ModelTrainingPipeline()
    model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
 except Exception as e:
         logger.exception(e)
         raise e
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -3,7 +3,8 @@ import os
 from cnnClassifier.utils.common import read_yaml, create_directories,save_json
 from cnnClassifier.entity.config_entity import (DataIngestionConfig,
                                                 PrepareBaseModelConfig,
-                                                TrainingConfig)
+                                                TrainingConfig,
+                                                EvaluationConfig)
 
 
 
@@ -76,3 +77,13 @@ class ConfigurationManager:
 
         return training_config
     
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/kidney-ct-scan-image",
+            mlflow_uri="https://dagshub.com/JunaidARahat/Kidney-Disease-Classification-Using-Deep-Learning.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
